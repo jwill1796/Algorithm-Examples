9b27823c27c23601496765b70c96f453377a02d4
jwill1796 <jwills1796@gmail.com>
2025-03-25 10:09:10 -0500

    Added Leetcode Problem 346: Moving Average from Data Stream

---
+++ LeetCodeHackerRankProbs/P346MovingAverageFromDataStream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LeetCodeHackerRankProbs
+{
+    public class MovingAverage
+    {
+        private Queue<int> _queue;
+        int size;
+        double sum;
+        public MovingAverage(int size)
+        {
+            _queue = new Queue<int>(size);
+            this.size = size;
+            sum = 0;
+        }
+        public double Next(int val)
+        {
+            _queue.Enqueue(val);
+            sum += val;
+            while (_queue.Count > size)
+            {
+                sum -= _queue.Dequeue();
+            }
+
+            return sum / _queue.Count;
+        }
+    }
+}
---
+++ LeetCodeHackerRankTest/P346MovingAverageFromDataStreamTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+using LeetCodeHackerRankProbs;
+
+namespace LeetCodeHackerRankTest
+{
+    public class P346MovingAverageFromDataStreamTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 10, 3, 5 }, 3, new double[] { 1.0, 5.5, 4.666666666666667, 6.0 })]
+        [InlineData(new int[] { 1, 2, 3, 4 }, 2, new double[] { 1.0, 1.5, 2.5, 3.5 })]
+        [InlineData(new int[] { 5, 5, 5, 5 }, 1, new double[] { 5.0, 5.0, 5.0, 5.0 })]
+        public void MovingAverageTest(int[] inputs, int size, double[] expected)
+        {
+            var movingAverage = new MovingAverage(size);
+            var results = new double[inputs.Length];
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                results[i] = movingAverage.Next(inputs[i]);
+            }
+
+            results.ShouldBe(expected);
+        }
+
+        
+    }
+}
