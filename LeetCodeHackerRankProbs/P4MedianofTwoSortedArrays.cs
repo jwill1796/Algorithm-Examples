ac7bd25e84d83b52550b4c78163d1b179e52be06
jwill1796 <jwills1796@gmail.com>
2025-03-25 11:13:47 -0500

    Added Leetcode Problem 4: Median of Two Sorted Arrays

---
+++ LeetCodeHackerRankProbs/P4MedianofTwoSortedArrays.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeHackerRankProbs
+{
+    public class P4MedianofTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] mergedArray = new int[nums1.Length + nums2.Length];
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < nums1.Length && j < nums2.Length)
+            {
+                if (nums1[i] < nums2[j])
+                {
+                    mergedArray[k++] = nums1[i++];
+                }
+                else
+                {
+                    mergedArray[k++] = nums2[j++];
+                }
+            }
+
+            while (i < nums1.Length)
+            {
+                mergedArray[k++] = nums1[i++];
+            }
+            while (j < nums2.Length)
+            {
+                mergedArray[k++] = nums2[j++];
+            }
+
+            int n = mergedArray.Length;
+            if (mergedArray.Length % 2 == 0)
+            {
+                return (mergedArray[n / 2] + mergedArray[n / 2 - 1]) / 2.0;
+            }
+            else
+            {
+                return mergedArray[n / 2];
+            }
+        }
+    }
+}
---
+++ LeetCodeHackerRankTest/P4MedianOfTwoSortedArraysTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+using LeetCodeHackerRankProbs;
+
+namespace LeetCodeHackerRankTest
+{
+    public  class P4MedianOfTwoSortedArraysTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2.0)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5)]
+
+        public void FindMedianSortedArraysTest(int[] nums1, int[] nums2, double expected)
+        {
+            var medianOfTwoSortedArrays = new P4MedianofTwoSortedArrays();
+            var result = medianOfTwoSortedArrays.FindMedianSortedArrays(nums1, nums2);
+            result.ShouldBe(expected);
+        }
+    }
+}
