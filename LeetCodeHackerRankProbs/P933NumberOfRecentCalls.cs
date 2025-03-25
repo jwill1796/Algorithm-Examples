87a0dabeb0b2e905573d5575a53f91103dc85eef
jwill1796 <jwills1796@gmail.com>
2025-03-25 10:38:05 -0500

    Added Leetcode problem 933: Number of Recent Calls

---
+++ LeetCodeHackerRankProbs/P933NumberOfRecentCalls.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeHackerRankProbs
+{
+    public class P933NumberOfRecentCalls
+    {
+        private Queue<int> _queue;
+        public P933NumberOfRecentCalls()
+        {
+            _queue = new Queue<int>();
+        }
+
+        public int Ping(int t)
+        {
+            _queue.Enqueue(t);
+            while (_queue.Peek() < t - 3000) 
+            { 
+                _queue.Dequeue();
+            }
+            return _queue.Count;
+        }
+    }
+}
---
+++ LeetCodeHackerRankTest/P933NumberOfRecentCallsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LeetCodeHackerRankProbs;
+using Shouldly;
+using Xunit;
+
+namespace LeetCodeHackerRankTest
+{
+    public class P933NumberOfRecentCallsTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 100, 3001, 3002 }, new int[] { 1, 2, 3, 3 })]
+        [InlineData(new int[] { 1, 100, 3001, 3002, 3003, 3004 }, new int[] { 1, 2, 3, 3, 4, 5 })]
+        public void PingTest(int[] inputs, int[] expected)
+        {
+            var numberOfRecentCalls = new P933NumberOfRecentCalls();
+            var results = new int[inputs.Length];
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                results[i] = numberOfRecentCalls.Ping(inputs[i]);
+            }
+            results.ShouldBe(expected);
+        }
+    }
+}
